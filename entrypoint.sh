5df69e099dfb79b20af3698d07f7e9862283206c
Andrew Chumich <andrewchumich@gmail.com>
2017-07-21 15:35:12 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,9 @@ fi
 echo ">> copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
 cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null
 
+echo ">> update CA certificates"
+update-ca-certificates
+
 # exec CMD
 echo ">> exec docker CMD"
 echo "$@"
